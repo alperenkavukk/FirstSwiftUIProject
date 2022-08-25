ea0888eaeab731a3d2d3fc7d7af85dff18ad685e
alperenkavukk <kavuk.67.76@gmail.com>
2022-08-25 18:38:19 +0300

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Alperen Kavuk on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
